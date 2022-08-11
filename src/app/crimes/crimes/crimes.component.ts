8e39a9b0095cc4d5935c0929c97f5f3f800adb4b
Mchim91 <chimenem.mbato99@gmail.com>
2022-08-11 21:43:29 +0100

    added crimes

--- src/app/crimes/crimes/crimes.component.ts
+++ src/app/crimes/crimes/crimes.component.ts
@@ -8,13 +8,78 @@ import { SmartSocietyServiceService } from 'src/app/shared/service/smart-society
   styleUrls: ['./crimes.component.css']
 })
 export class CrimesComponent implements OnInit {
+  openModal='none';
+  openModal2 = 'none';
+  duration =0;
+  main = [] as any;
+  data = [] as any;
+
+  header = [
+    { key: 'crimeIdentificationNo', label: 'Crime Identification Number'},
+    { key: 'crimeCategoryId', label: 'Crime Category'},
+    { key: 'crimeSubCategoryId', label: 'Crime Sub Category'},
+    { key: 'crimeType', label: 'Crime Type'},
+    { key: 'crimeSubType', label: 'Crime Sub Type'},
+    { key: 'crimeName', label: 'Crime Name'},
+  ];
   
   constructor(private http: SmartSocietyServiceService) { }
 
   ngOnInit(): void {
     const a = this.http.findAll('crimes').subscribe((res:any) => {
-      console.log(res)
-
+      this.data = res;
     });
   }
+
+  crimesForm = new FormGroup({
+    crimeIdentificationNo: new FormControl('',[ Validators.required]),
+    crimeCategoryId: new FormControl('',[ Validators.required]),
+    crimeSubCategoryId: new FormControl('',[ Validators.required]),
+    crimeType: new FormControl('',[ Validators.required]),
+    crimeSubType: new FormControl('',[ Validators.required]),
+    crimeName: new FormControl('',[ Validators.required]),
+  });
+
+  onClickOpenModalAdd(){
+
+    this.openModal='block'
+  
+  }
+
+  onClickCloseModalAdd(){
+    this.openModal='none'
+  }
+
+  onSubmit(){
+    this.http.create(this.crimesForm.value, 'crimes').subscribe((res) => {
+      console.log(res);
+      this.crimesForm.reset();
+      this.openModal='none'
+    });
+
+  }
+
+  onEdit(value: any){
+    this.openModal2='none';
+    this.crimesForm.setValue({
+      crimeIdentificationNo: value?.crimeIdentificationNo,
+      crimeCategoryId: value?.crimeCategoryId,
+      crimeSubCategoryId: value?.crimeSubCategoryId,
+      crimeType: value?.crimeType,
+      crimeSubType: value?.crimeSubType,
+      crimeName: value?.crimeName,
+    });
+    this.main = value;
+  }
+
+  onDelete(value: any){
+    if(!value) {
+      console.log('not found');
+      return;
+    }
+    this.http.delete(value, 'prisonward').subscribe((res) =>{});
+    window.location.reload();
+  }
+
+
 }
