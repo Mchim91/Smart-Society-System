7b3819fc52f52a7b13f1aecb460a167d7c64f27a
Mchim91 <chimenem.mbato99@gmail.com>
2022-08-11 12:15:21 +0100

    latest update

--- src/app/prison-ward/prisonward/prisonward.component.ts
+++ src/app/prison-ward/prisonward/prisonward.component.ts
@@ -22,11 +22,6 @@ export class PrisonwardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
-    const a = this.http.findAll('prisonward').subscribe((res:any) => {
-      console.log(res)
-
-    });
     this.http.findAll('prisonward').subscribe(res=>{
       this.PRISONWARD=res
     })
