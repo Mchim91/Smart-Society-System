c5af27c818744a927e470c65c1a42bf97a47c9a8
AsukFavour <favourasuk@gmail.com>
2022-08-12 09:28:15 +0100

    added table for c.c

--- src/app/correction-centers/correctioncenters/correctioncenters.component.ts
+++ src/app/correction-centers/correctioncenters/correctioncenters.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SmartSocietyServiceService } from 'src/app/shared/service/smart-society-service.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-correctioncenters',
@@ -7,14 +9,78 @@ import { SmartSocietyServiceService } from 'src/app/shared/service/smart-society
   styleUrls: ['./correctioncenters.component.css']
 })
 export class CorrectioncentersComponent implements OnInit {
+  openModal='none';
+  openModal2 = 'none';
+  duration=0;
+  main = [] as any;
+  data = [] as any;
+
+  header = [
+    { key: 'name', label: 'Name'},
+    { key: 'prisonType', label: 'Prison Type'}
+  ];
 
   constructor(private http: SmartSocietyServiceService) { }
 
   ngOnInit(): void {
     const a = this.http.findAll('correctioncenters').subscribe((res:any) => {
+      this.data = res;
       console.log(res)
 
     });
   }
 
+  correctioncenterForm = new FormGroup({
+
+    name: new FormControl('', [Validators.required]),
+    prisonType: new FormControl('', [Validators.required]),
+  });
+
+  onClickOpenModalAdd(){
+
+    this.openModal='block'
+  
+  }
+
+  onClickOpenModalAdd2(){
+     this.openModal2='block'
+  }
+
+  onClickCloseModalAdd(){
+    this.openModal='none'
+  }
+
+  onUpdate() {
+    const a = this.http
+      .update(this.main?._id, [this.correctioncenterForm.value], 'correctioncenters')
+      .subscribe((res) => {});
+    this.openModal2 = 'none';
+    window.location.reload();
+  }
+  onSubmit(){
+    this.http.create(this.correctioncenterForm.value, 'correctioncenters').subscribe((res) => {
+      console.log(res);
+      this.correctioncenterForm.reset();
+      this.openModal='none'
+    });
+
+  }
+
+  onEdit(value: any){
+    this.openModal2='none';
+    this.correctioncenterForm.setValue({
+      name: value?.name,
+      prisonType: value?.prisonType
+    });
+    this.main = value;
+  }
+
+  onDelete(value: any){
+    if(!value) {
+      console.log('not found');
+      return;
+    }
+    this.http.delete(value, 'correctioncenters').subscribe((res) =>{});
+    window.location.reload();
+  }
 }
